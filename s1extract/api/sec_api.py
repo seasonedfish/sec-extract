d4c860cb404c5d4e88ef17f8be9357024e66f08a
Fisher Sun <fisher521.fs@gmail.com>
2022-05-30 14:34:32 -0400

    Use sec-api

--- s1extract/api/sec_api.py
+++ s1extract/api/sec_api.py
@@ -1,5 +1,6 @@
 import csv
 from collections import namedtuple
+from sec_api import QueryApi
 
 from s1extract.api.keys import SEC_API_KEY
 
@@ -17,7 +18,20 @@ def get_firms():
 
 
 def main():
-    pass
+    query_api = QueryApi(api_key=SEC_API_KEY)
+    query = {
+        "query": {
+            "query_string": {
+                "query": "ticker:(AAOI) AND formType:\"S-1\""
+            }
+        },
+        "from": "0",
+        "size": "1",
+        "sort": [{"filedAt": {"order": "desc"}}]
+    }
+    filings = query_api.get_filings(query)
+
+    print(filings)
 
 
 if __name__ == "__main__":
