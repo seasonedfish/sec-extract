874a07bd443d267ab8c6b6ea04d1670881d4459a
Fisher Sun <fisher521.fs@gmail.com>
2022-06-01 15:59:27 -0400

    Replace pypi sec-api with local package

---
+++ sec_api/__init__.py
@@ -0,0 +1,7 @@
+name = "sec_api"
+from sec_api.index import QueryApi
+from sec_api.index import FullTextSearchApi
+from sec_api.index import RenderApi
+from sec_api.index import XbrlApi
+from sec_api.index import ExtractorApi
+from sec_api.index import MappingApi
