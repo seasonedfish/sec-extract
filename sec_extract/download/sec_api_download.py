4b86afc1c58eb373db381ef8465c6cabc8ce5c06
Fisher Sun <fisher521.fs@gmail.com>
2022-06-06 15:52:28 -0400

    Add code for 10-K

--- sec_extract/download/sec_api_download.py
+++ sec_extract/download/sec_api_download.py
@@ -1,6 +1,7 @@
 import csv
 import sys
-from pathlib import Path
+from collections import namedtuple
+from os import path
 
 from sec_api import QueryApi, RenderApi
 
@@ -12,19 +13,26 @@ QUERY_API = QueryApi(SEC_API_KEY)
 RENDER_API = RenderApi(SEC_API_KEY)
 
 
-def get_tickers() -> list[str]:
+class FormNotFoundError(Exception):
+    pass
+
+
+Firm = namedtuple("Firm", ("ticker_symbol", "year", "cusip"))
+
+
+def get_firms() -> list[Firm]:
     with open("IPO Firm list 2005-2019.csv") as f:
         reader = csv.reader(f)
         reader.__next__()  # Skip first row
-        firms = [row[0] for row in reader]  # Get only the ticker
+        firms = [Firm(*row) for row in reader]
     return firms
 
 
-def get_url(ticker: str, form_type: str) -> str:
+def get_s1_url(ticker: str) -> str:
     query = {
         "query": {
             "query_string": {
-                "query": f"ticker:{ticker} AND formType:\"{form_type}\""
+                "query": f"ticker:{ticker} AND formType:\"S-1\""
             }
         },
         "from": "0",
@@ -35,26 +43,37 @@ def get_url(ticker: str, form_type: str) -> str:
     try:
         return filings["filings"][0]["linkToFilingDetails"]
     except IndexError:
-        raise ValueError(f"No {form_type} found for {ticker}")
+        raise FormNotFoundError(f"No S-1 found for {ticker}")
 
 
-def download_html(ticker: str, form_type: str) -> None:
-    destination_path = Path(
-        f"{form_type.replace('-', '').lower()}_html/{ticker}.html"
-    )
-    if destination_path.exists():
-        return
-
+def get_10k_url(ticker: str, year: int) -> str:
+    year_range = f"[{year}-01-01T00:00:00 TO {year + 1}-01-01T00:00:00]"
+    query = {
+        "query": {
+            "query_string": {
+                "query": f"ticker:{ticker} AND formType:\"10-K\" AND filedAt: {year_range}",
+                "time_zone": "America/New_York"
+            }
+        },
+        "from": "0",
+        "size": "1",
+        "sort": [{"filedAt": {"order": "desc"}}]
+    }
+    filings = QUERY_API.get_filings(query)
     try:
-        url = get_url(ticker, form_type)
-    except ValueError as e:
-        print(e, file=sys.stderr)
+        return filings["filings"][0]["linkToFilingDetails"]
+    except IndexError:
+        raise FormNotFoundError(f"No 10-K found for {ticker} in range {year_range}")
+
+
+def download_html(url: str, destination_path: str) -> None:
+    if path.exists(destination_path):
         return
 
     try:
         html_string = RENDER_API.get_filing(url)
     except ConnectionError:
-        print(f"Could not download html for {ticker}", file=sys.stderr)
+        print(f"Could not download {url}", file=sys.stderr)
         return
 
     with open(destination_path, "w") as f:
@@ -62,9 +81,22 @@ def download_html(ticker: str, form_type: str) -> None:
 
 
 def main() -> None:
-    tickers = get_tickers()
-    for ticker in tickers:
-        download_html(ticker, "S-1")
+    firms = get_firms()
+    for firm in firms:
+        try:
+            url_s1 = get_s1_url(firm.ticker_symbol)
+        except FormNotFoundError as e:
+            print(e, file=sys.stderr)
+            continue
+        download_html(url_s1, f"s1_html/{firm.ticker_symbol}.html")
+
+    for firm in firms:
+        try:
+            url_10k = get_10k_url(firm.ticker_symbol, firm.year)
+        except FormNotFoundError as e:
+            print(e, file=sys.stderr)
+            continue
+        download_html(url_10k, f"10k_html/{firm.ticker_symbol}.html")
 
 
 if __name__ == "__main__":
