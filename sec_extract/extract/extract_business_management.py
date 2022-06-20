d7036a3038d6ba3e111fc52fc6a70990bb78fe81
Fisher Sun <fisher521.fs@gmail.com>
2022-06-20 15:26:15 -0400

    Factor out code into functions

--- sec_extract/extract/extract_business_management.py
+++ sec_extract/extract/extract_business_management.py
@@ -1,18 +1,36 @@
 from bs4 import BeautifulSoup
 
 
+def extract_sections(soup: BeautifulSoup) -> str:
+    return extract_business_section(soup) + extract_management_section(soup)
+
+
+def extract_between_tags(soup: BeautifulSoup, start_tag, end_tag) -> str:
+    soup_string = str(soup)
+    start_index = soup_string.index(str(start_tag))
+    end_index = soup_string.index(str(end_tag))
+    return soup_string[start_index: end_index]
+
+
+def extract_business_section(soup: BeautifulSoup) -> str:
+    start_anchor = soup.find("a", attrs={"name": "tx319036_10"})
+    end_anchor = soup.find("a", attrs={"name": "tx319036_11"})
+
+    return extract_between_tags(soup, start_anchor, end_anchor)
+
+
+def extract_management_section(soup: BeautifulSoup) -> str:
+    start_anchor = soup.find("a", attrs={"name": "tx319036_11"})
+    end_anchor = soup.find("a", attrs={"name": "tx319036_12"})
+
+    return extract_between_tags(soup, start_anchor, end_anchor)
+
+
 def main():
     with open("/Users/fisher/PycharmProjects/sec-extract/sec_extract/download/s1_html/GMED.html") as f:
         soup = BeautifulSoup(f, "html.parser")
 
-    business_anchor = soup.find("a", attrs={"name": "tx319036_10"})
-    management_end_anchor = soup.find("a", attrs={"name": "tx319036_12"})
-
-    soup_text = str(soup)
-    start_index = soup_text.index(str(business_anchor))
-    stop_index = soup_text.index(str(management_end_anchor))
-
-    result = soup_text[start_index: stop_index]
+    result = extract_sections(soup)
 
     with open("/Users/fisher/PycharmProjects/sec-extract/sec_extract/extract/s1/GMED.html", "w") as f:
         f.write(result)
