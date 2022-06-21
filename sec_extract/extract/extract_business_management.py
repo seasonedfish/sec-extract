d62e72eaaaccdf5e690e3869132b7d38c585d12d
Fisher Sun <fisher521.fs@gmail.com>
2022-06-21 14:49:39 -0400

    Use HTML 4 charset specification

--- sec_extract/extract/extract_business_management.py
+++ sec_extract/extract/extract_business_management.py
@@ -1,10 +1,10 @@
 from bs4 import BeautifulSoup
 import functools
 
-BEFORE = """<!DOCTYPE html>
+BEFORE = """<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
