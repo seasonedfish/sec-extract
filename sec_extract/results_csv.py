2473fe03cedfb4e9bedd4e140fd143086c94fa54
Fisher Sun <fisher521.fs@gmail.com>
2022-07-06 16:04:44 -0400

    Add PDF columns to results.csv
    Signed-off-by: Fisher Sun <fisher521.fs@gmail.com>

--- sec_extract/results_csv.py
+++ sec_extract/results_csv.py
@@ -10,18 +10,29 @@ def write_results(writer: DictWriter, firms: list[Firm]) -> None:
     for firm in firms:
         has_s1_business = Path(f"./target/s1_business/{firm.ticker_symbol}.html").exists()
         has_s1_management = Path(f"./target/s1_management/{firm.ticker_symbol}.html").exists()
-
         number_of_10ks = len(
             list(
                 Path(f"./target/10k_html").glob(f"{firm.ticker_symbol}{'[0-9]' * 4}.html")
             )
         )
+
+        pdf_has_s1_business = Path(f"./target/pdf/s1_business/{firm.ticker_symbol}.pdf").exists()
+        pdf_has_s1_management = Path(f"./target/pdf/s1_management/{firm.ticker_symbol}.pdf").exists()
+        pdf_number_of_10ks = len(
+            list(
+                Path(f"./target/pdf/10k_html").glob(f"{firm.ticker_symbol}{'[0-9]' * 4}.pdf")
+            )
+        )
+
         writer.writerow(
             {
                 "Ticker": firm.ticker_symbol,
                 "Has S-1 Business": int(has_s1_business),
                 "Has S-1 Management": int(has_s1_management),
-                "Number of 10-Ks": number_of_10ks
+                "Number of 10-Ks": number_of_10ks,
+                "PDF Has S-1 Business": int(pdf_has_s1_business),
+                "PDF Has S-1 Management": int(pdf_has_s1_management),
+                "PDF Number of 10-Ks": pdf_number_of_10ks
             }
         )
 
@@ -39,7 +50,10 @@ def main():
                 "Ticker",
                 "Has S-1 Business",
                 "Has S-1 Management",
-                "Number of 10-Ks"
+                "Number of 10-Ks",
+                "PDF Has S-1 Business",
+                "PDF Has S-1 Management",
+                "PDF Number of 10-Ks"
             ]
         )
 
